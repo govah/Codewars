f784db8843f0f9a262a68ccf95ded9e23c4db4b5
Alirezagovah <ialirezagovah@gmail.com>
2021-11-28 12:20:05 +0330

    Change the 8 kyu

---
+++ 8 kyu/Convert boolean values to strings 'Yes' or 'No'./script.js
@@ -0,0 +1,13 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+function even_or_odd(number) {
+  if(number % 2 ){
+    return 'Odd';
+  }
+  else{
+    return 'Even'
+   }
+}
+
+even_or_odd();
