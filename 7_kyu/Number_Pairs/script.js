bc438f9f2da4c46c275d7daef8352d63d793b0e8
govah <ialirezagovah@gmail.com>
2022-03-03 20:12:06 +0330

    add Number_Pairs

---
+++ 7_kyu/Number_Pairs/script.js
@@ -0,0 +1,17 @@
+/*In this Kata the aim is to compare each pair of integers from 2 arrays, and return a new array of large numbers.
+
+Note: Both arrays have the same dimensions.
+
+Example:
+
+let arr1 = [13, 64, 15, 17, 88];
+let arr2 = [23, 14, 53, 17, 80];
+getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]*/
+
+function getLargerNumbers(a, b) {
+  var newl = []
+  for(i=0;i<a.length;i++){
+    newl.push(Math.max(a[i],b[i]))
+  }
+  return newl;
+}
